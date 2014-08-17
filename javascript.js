93d2b2da1e8cefa1322ea60549c7b398338e2578
Kenji Miwa <kenjim83@gmail.com>
2014-08-16 17:23:41 -0700

    working on slider

---
+++ javascript.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+  sliderIndex = 0;
+
+  bindListeners();
+});
+
+
+
+function bindListeners(){
+  $('#prev-arrow').click(function(e){
+    e.preventDefault();
+
+    if(sliderIndex === 0){ sliderIndex = slides.length-1 }
+    sliderIndex -= 1;
+
+    var textContent = slides[sliderIndex].html;
+    var imgLink = slides[sliderIndex].image;
+
+    $('#slider-text').html(textContent);
+
+    // var bgCSS = "url(" + imgLink + ") no-repeat"
+
+    $('body').css( "background" , "url(" + imgLink + ") no-repeat" );
+  })
+
+  $('#next-arrow').click(function(){
+    e.preventDefault();
+
+
+     if(sliderIndex+1 === slides.length){ sliderIndex = 0; }
+    $('#slider-text').html("<h1>THIS HAS BEEN CHANGED</h1>")
+  })
+
+}
+
+
+
+
+slides = [
+  { html: "<h1>Welcome to M Grill</h1><br><p>Come see the newly remodeled and much larger M Grill interior.</p>",
+    image: "images/diningroom.jpg"
+  },
+
+  { html: "<h1>In the Heart of Los Angeles</h1><br><p>Near Downtown LA, Hollywood, the beaches, and more...</p>",
+    image: "images/slider_LA.jpg"
+  },
+
+  { html: "<h1>Freshest Premium Meats</h1><br><p>Expertly Grilled Over An Open Charcoal Grill</p>",
+    image: "images/slider_meats.jpg"
+  }
+
+]
